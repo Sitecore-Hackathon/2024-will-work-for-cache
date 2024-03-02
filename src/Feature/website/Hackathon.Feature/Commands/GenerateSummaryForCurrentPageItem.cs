c8220f3c8a8d7260e0bd77f97ed121c61682527f
jeremyhaun <jeremh@gmail.com>
2024-03-02 09:48:19 -0600

    adjusting summary generation to check for site info and base template, adding keys to config

--- src/Feature/website/Hackathon.Feature/Commands/GenerateSummaryForCurrentPageItem.cs
+++ src/Feature/website/Hackathon.Feature/Commands/GenerateSummaryForCurrentPageItem.cs
@@ -9,15 +9,21 @@ using Sitecore.Links;
 using Sitecore;
 using HtmlAgilityPack;
 using System.Text;
+using Sitecore.Configuration;
+using Sitecore.Web;
+using Sitecore.XA.Foundation.Multisite;
+using Sitecore.Data.Managers;
+using Sitecore.Data;
 
 
 namespace Hackathon.Feature.Commands
 {
     public class GenerateSummaryForCurrentPageItem : Command
     {
-        // TODO: remove keys and use env variables
-        static string languageKey = "cf708cb08fc146b18a22ff96015fd1ac";
-        static string languageEndpoint = "https://hackathonlanguageanalysis.cognitiveservices.azure.com/";
+
+        static string languageKey => Settings.GetSetting("LANGUAGE_KEY");
+        static string languageEndpoint => Settings.GetSetting("LANGUAGE_ENDPOINT");
+        static string localizedCMurl => Settings.GetSetting("LOCALIZED_CM_URL");
 
         private static readonly AzureKeyCredential credentials = new AzureKeyCredential(languageKey);
         private static readonly Uri endpoint = new Uri(languageEndpoint);
@@ -26,18 +32,41 @@ namespace Hackathon.Feature.Commands
         {
             if (context.Items.Length != 1)
             {
-                Sitecore.Context.ClientPage.ClientResponse.Alert("Unable to determine the current item");
+                Context.ClientPage.ClientResponse.Alert("Unable to determine the current item");
                 return;
             }
 
-            // TODO: do something better with the url replacement - this will only work in containers, and with a single site
-            string original = LinkManager.GetItemUrl(context.Items[0]);
-            var itemPageUrl = GetLocalUrl(original, "127.0.0.1");
+            var contextItem = context.Items[0];
+            //if item is not a page item, return
+            if (!TemplateManager.GetTemplate(contextItem).InheritsFrom(new ID(Constants.PageBaseTemplate.TemplateId)))
+            {
+                Context.ClientPage.ClientResponse.Alert("Item is not a page and cannot have a summary generated.");
+                return;
+            }
+
+
+            // get site info from page item to make sure item is within a site and to append to the url to handle multisite
+            string siteName = GetSiteInfoFromPath(contextItem);
+            if (string.IsNullOrEmpty(siteName))
+            {
+                Context.ClientPage.ClientResponse.Alert("Item is not within a site and cannot be rendered to generate a summary.");
+                return;
+            }
+
+            //get url from localized setting and site querystring param
+            string original = LinkManager.GetItemUrl(contextItem);
+            var itemPageUrl = GetLocalUrl(original, localizedCMurl, siteName);
 
             var pageString = Sitecore.Web.WebUtil.ExecuteWebPage(itemPageUrl);
 
             //parse text content out of html from rendered page
             var parsedPageContent = GetTrimmedHTML(pageString);
+
+            if (string.IsNullOrEmpty(parsedPageContent))
+            {
+                Context.ClientPage.ClientResponse.Alert("There is no content on the page so a summary cannot be generated.");
+                return;
+            }
 
             string pageSummary;
             try
@@ -50,7 +79,7 @@ namespace Hackathon.Feature.Commands
             }
             catch (Exception e)
             {
-                Sitecore.Context.ClientPage.ClientResponse.ShowError(e);
+                Context.ClientPage.ClientResponse.ShowError(e);
                 return;
             }
 
@@ -131,13 +160,21 @@ namespace Hackathon.Feature.Commands
         /// <param name="item"></param>
         /// <param name="newHostName"></param>
         /// <returns></returns>
-        private string GetLocalUrl(string original, string newHostName)
+        private string GetLocalUrl(string original, string newHostName, string siteName)
         {
             var builder = new UriBuilder(original);
             builder.Host = newHostName;
             builder.Scheme = Uri.UriSchemeHttp;
             builder.Port = -1;
-            return builder.Uri.ToString();
+            return builder.Uri.ToString() + "?sc_site=" + siteName;
+        }
+
+        private string GetSiteInfoFromPath(Sitecore.Data.Items.Item item)
+        {
+            SiteInfoResolver resolver = new SiteInfoResolver();
+            SiteInfo parentSite = resolver.GetSiteInfo(item);
+
+            return parentSite?.Name ?? string.Empty;
         }
     }
 }
---
+++ src/Feature/website/Hackathon.Feature/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hackathon.Feature
+{
+    public class Constants
+    {
+        public static class PageBaseTemplate
+        {
+            public static readonly Guid TemplateId = new Guid("{3F8A6A5D-7B1A-4566-8CD4-0A50F3030BD8}");
+        }
+    }
+}
