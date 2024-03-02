169250c81098429ed915c408aa01fda4e5e636eb
Matt Taylor <matt@geekrocket.com>
2024-03-02 01:00:19 -0500

    Alt text scaffolding

--- src/Feature/website/Hackathon.Feature/Commands/GenerateAltTextForCurrentMediaItem.cs
+++ src/Feature/website/Hackathon.Feature/Commands/GenerateAltTextForCurrentMediaItem.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Web;
 using Azure;
 using Azure.AI.Vision.ImageAnalysis;
 using Sitecore.Configuration;
 using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
 
 namespace Hackathon.Feature.Commands
 {
@@ -10,9 +12,11 @@ namespace Hackathon.Feature.Commands
     {
         public override void Execute(CommandContext context)
         {
+            var response = Sitecore.Context.ClientPage.ClientResponse;
+
             if (context.Items.Length != 1)
             {
-                Sitecore.Context.ClientPage.ClientResponse.Alert("Unable to determine the current item");
+                response.Alert("Unable to determine the current item");
                 return;
             }
 
@@ -21,44 +25,63 @@ namespace Hackathon.Feature.Commands
             var media = Sitecore.Resources.Media.MediaManager.GetMedia(item);
             if (media == null)
             {
-                Sitecore.Context.ClientPage.ClientResponse.Alert($"Unable to retrieve media for item: {item.ID}");
+                response.Alert($"Unable to retrieve media for item: {item.ID}");
                 return;
             }
 
             var visionEndpoint = Settings.GetSetting("VISION_ENDPOINT");
             if (string.IsNullOrWhiteSpace(visionEndpoint))
             {
-                Sitecore.Context.ClientPage.ClientResponse.Alert($"The VISION_ENDPOINT setting did not have a value");
+                response.Alert($"The VISION_ENDPOINT setting did not have a value");
                 return;
             }
 
             var visionKey = Settings.GetSetting("VISION_KEY");
             if (string.IsNullOrWhiteSpace(visionKey))
             {
-                Sitecore.Context.ClientPage.ClientResponse.Alert($"The VISION_KEY setting did not have a value");
+                response.Alert($"The VISION_KEY setting did not have a value");
                 return;
-            }
-
-            // Read media stream to buffer
-            byte[] buffer;
-            using (var mediaStream = media.GetStream())
-            {
-                buffer = new byte[mediaStream.Length];
-                mediaStream.Stream.Read(buffer, 0, buffer.Length);
             }
 
             try
             {
-                ImageAnalysisClient client = new ImageAnalysisClient(new Uri(visionEndpoint), new AzureKeyCredential(visionKey));
+                var client = new ImageAnalysisClient(new Uri(visionEndpoint), new AzureKeyCredential(visionKey));
+
+                byte[] buffer;
+                using (var mediaStream = media.GetStream())
+                {
+                    buffer = new byte[mediaStream.Length];
+                    mediaStream.Stream.Read(buffer, 0, buffer.Length);
+                }
 
                 ImageAnalysisResult result = client.Analyze(new BinaryData(buffer), VisualFeatures.Caption);
 
-                Sitecore.Context.ClientPage.ClientResponse.Alert($"   '{result.Caption.Text}', Confidence {result.Caption.Confidence:F4}");
+                var caption = result.Caption.Text;
+
+                if (string.IsNullOrWhiteSpace(caption))
+                {
+                    response.Alert("Sorry, no caption could be generated for this image.");
+                    return;
+                }
+
+                // Caption comes back all lowercase, so uppercase the first character
+                caption = caption.Substring(0, 1).ToUpper() + caption.Substring(1);
+
+                response.Eval($"window.willWorkForCache.setFieldValueByName(\"Alt\", \"{HttpUtility.JavaScriptStringEncode(caption)}\", true);");
             }
             catch (Exception e)
             {
-                Sitecore.Context.ClientPage.ClientResponse.ShowError(e);
-                return;
+                if (e is AggregateException agg)
+                {
+                    foreach (var ex in agg.InnerExceptions)
+                    {
+                        response.ShowError(ex);
+                    }
+                }
+                else
+                {
+                    response.ShowError(e);
+                }
             }
         }
     }
---
+++ src/Feature/website/Hackathon.Feature/Pipelines/RenderContentEditor/AddAssistantScripts.cs
@@ -0,0 +1,40 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Pipelines;
+using Sitecore.StringExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using Sitecore.Configuration;
+using Sitecore.Resources;
+
+namespace Hackathon.Feature.Pipelines.RenderContentEditor
+{
+    public class AddAssistantScripts
+    {
+        // Inspired by https://jammykam.wordpress.com/2014/04/24/adding-custom-javascript-and-stylesheets-in-the-content-editor/
+
+        public void Process(PipelineArgs args)
+        {
+            var resources = Settings.GetSetting("WillCodeForCacheScriptFiles");
+
+            if (string.IsNullOrEmpty(resources))
+                return;
+
+            var builder = new StringBuilder();
+            builder.AppendLine($"<!-- Start {nameof(AddAssistantScripts)} scripts -->");
+
+            foreach (var resource in resources.Split('|'))
+            {
+                builder.AppendFormat("<script type=\"text/javascript\" src=\"{0}\"></script>\r\n", resource);
+            }
+
+            builder.AppendLine($"<!-- End {nameof(AddAssistantScripts)} scripts -->");
+
+            Sitecore.Context.Page.Page.Header.Controls.Add(new LiteralControl(builder.ToString()));
+
+        }
+    }
+}
